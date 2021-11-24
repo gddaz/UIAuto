5e3eaa3404ab53c8c23d7aa0985e7f168a69f516
gddaz <1010622677@qq.com>
2021-11-24 14:23:58 +0800

    test

---
+++ public/page_obj/loginPage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+__author__ = 'HD'
+
+import os,sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+from config import setting
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from public.page_obj.base import Page
+from time import sleep
+from public.models.GetYaml import getyaml
+
